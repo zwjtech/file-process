2d3b2fd18a46d07d0e0e2a0d9cc7e948f7b80da5
zhangweijie@jiagouyun.com <Hello1234>
2019-01-02 23:01:24 +0800

    init

--- src/test/java/file/FileIoTest.java
+++ src/test/java/file/FileIoTest.java
@@ -2,6 +2,7 @@ package file;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -13,7 +14,45 @@ public class FileIoTest {
     @Test
     public void fileReaderTest() throws IOException {
         FileReader fr = new FileReader("E:\\sourceCode\\file-process\\src\\test\\java\\file\\buf.txt");
+        //读一个
         int readNum = fr.read();
-        System.out.println(readNum);
+        System.out.print(readNum);
+        System.out.print((char)readNum);
+        //读下一个
+        int readNum1 = fr.read();
+        System.out.print(readNum1);
+        System.out.print((char)readNum1);
+
+        int chNum = 0;
+        while( chNum != -1){
+            chNum = fr.read();
+            System.out.print((char)chNum);
+        }
+        fr.close();
     }
+
+    @Test
+    public void fileReaderTest1() throws IOException {
+        FileReader fr = new FileReader("E:\\sourceCode\\file-process\\src\\test\\java\\file\\buf.txt");
+        // type arrayName[] = new type[arraySize];
+        char buf[] = new char[1024];
+        while( fr.read(buf) != -1){
+            System.out.print(new String(buf));
+        }
+        fr.close();
+    }
+
+    @Test
+    public void bufferedFileReaderTest() throws IOException {
+        BufferedReader in = new BufferedReader(
+                new FileReader("E:\\sourceCode\\file-process\\src\\test\\java\\file\\buf.txt"));
+        String line = null;
+        while ((line = in.readLine()) !=null){
+            System.out.print(line);
+        }
+
+        in.close();
+    }
+
+
 }
